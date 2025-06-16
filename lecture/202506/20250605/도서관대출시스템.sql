4ee766e7995fc24147fad9b8fab8e9787ecc80b0
mypsyche98 <mypsyche98@gmail.com>
2025-06-16 16:28:35 +0900

    Add files via upload

---
+++ lecture/202506/20250605/도서관대출시스템.sql
@@ -0,0 +1,14 @@
+﻿DROP TABLE IF EXISTS `Members`;
+
+CREATE TABLE `Members` (
+	`member_id`	VARCHAR(10)	NOT NULL,
+	`name`	VARCHAR(512)	NULL,
+	`birth_year`	INT(4)	NULL,
+	`gender`	VARCHAR(10)	NULL,
+	`mod_dts`	DATE	NULL
+);
+
+ALTER TABLE `Members` ADD CONSTRAINT `PK_MEMBERS` PRIMARY KEY (
+	`member_id`
+);
+
